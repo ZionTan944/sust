68f3e30885f690bc3cb54cac309217704cc142df
Zion <ziontanyien@gmail.com>
2025-10-21 17:59:46 +0800

    setup frontend remove init

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="navbar fixed-bottom row d-flex">
+        <div class="col-3"></div>
+
+        <div class="col-2">
+          <RouterLink class="nav-item" to="/d">
+            <font-awesome-icon icon="fa-solid fa-utensils" />
+          </RouterLink>
+        </div>
+        <div class="col-2">
+          <RouterLink class="nav-item" to="/d">
+            <font-awesome-icon icon="fa-solid fa-people-group" />
+          </RouterLink>
+        </div>
+        <div class="col-2">
+          <RouterLink class="nav-item" to="/d">
+            <font-awesome-icon icon="fa-solid fa-user" />
+          </RouterLink>
+        </div>
+        <div class="col-3"></div>
+  </nav>
+</template>
+<style scoped>
+/* Optional: customize the navbar colors */
+nav {
+  background-color: #dff7e2;
+}
+.nav-item {
+  color: black;
+}
+.navbar-brand {
+  font-weight: 600;
+  letter-spacing: 0.5px;
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,24 @@
 import './assets/main.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import 'bootstrap/dist/css/bootstrap.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
+library.add(fas, far, fab)
+
 const app = createApp(App)
-
 app.use(router)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
+
