aaf1f4e0cd850e86438991d65db2a7d6d20e1017
Zion <ziontanyien@gmail.com>
2025-10-22 20:04:03 +0800

    navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -3,31 +3,36 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <nav class="navbar fixed-bottom row d-flex">
-        <div class="col-3"></div>
+  <nav class="navbar fixed-bottom row d-flex p-0 ">
+    <div class="col-3"></div>
 
-        <div class="col-2">
-          <RouterLink class="nav-item" to="/d">
-            <font-awesome-icon icon="fa-solid fa-utensils" />
-          </RouterLink>
-        </div>
-        <div class="col-2">
-          <RouterLink class="nav-item" to="/d">
-            <font-awesome-icon icon="fa-solid fa-people-group" />
-          </RouterLink>
-        </div>
-        <div class="col-2">
-          <RouterLink class="nav-item" to="/d">
-            <font-awesome-icon icon="fa-solid fa-user" />
-          </RouterLink>
-        </div>
-        <div class="col-3"></div>
+    <RouterLink class="nav-item col-2 d-flex justify-content-center" to="/operator">
+      <div class="nav-box p-2 m-1">
+        <font-awesome-icon icon="fa-solid fa-utensils" />
+      </div>
+    </RouterLink>
+    <RouterLink class="nav-item col-2 d-flex justify-content-center" to="/leaderboard">
+      <div class="nav-box p-2 m-1">
+        <font-awesome-icon icon="fa-solid fa-people-group" />
+      </div>
+    </RouterLink>
+    <RouterLink class="nav-item col-2 d-flex justify-content-center" to="/profile">
+      <div class="nav-box p-2 m-1">
+        <font-awesome-icon icon="fa-solid fa-user" />
+      </div>
+    </RouterLink>
+    <div class="col-3"></div>
   </nav>
 </template>
 <style scoped>
-/* Optional: customize the navbar colors */
 nav {
   background-color: #dff7e2;
+  border-top-left-radius: 50px;
+  border-top-right-radius: 50px;
+}
+.nav-box:hover {
+  background-color: #00D09E;
+  border-radius: 25%;
 }
 .nav-item {
   color: black;
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  ds
+  Test
 </div>
 
 </template>
